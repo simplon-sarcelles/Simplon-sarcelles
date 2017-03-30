c31db1e976ebe9ba4a8eb2f8b1388735d865b48d
DDieudonne <lando-dieudonne@hotmail.fr>
2017-03-30 09:53:11 +0200

    ajout effets icons debouches

--- effets.js
+++ effets.js
@@ -1,1 +1,29 @@
-$('.min-icons').effect( "bounce", {times:3}, 300 );
+$( document ).ready(function() {
+  $('.icons1').mouseover(function(){
+    $('.infos1').stop().fadeIn('fade-infos');
+    $('.icons1').mouseout(function(){
+      $('.infos1').stop().fadeOut('fade-infos');
+      $('body').load(function(){
+        if ( $(this).height() > 500) {
+          $(this).addClass('min-icons');
+        }
+      });
+    });
+  });
+
+  $('.icons2').mouseover(function(){
+    $('.infos2').stop().fadeIn('fade-infos');
+    $('.icons2').mouseout(function(){
+      $('.infos2').stop().fadeOut('fade-infos');
+    });
+  });
+  $('.icons3').mouseover(function(){
+    $('.infos3').stop().fadeIn('fade-infos');
+    $('.icons3').mouseout(function(){
+      $('.infos3').stop().fadeOut('fade-infos');
+    });
+  });
+    $('.overlay').mouseover(function(){
+        $('.images-hover').fadeIn();
+    });
+});
