8a038c18cf242e015d14b1ff0d2aecb6a9444e38
DDieudonne <lando-dieudonne@hotmail.fr>
2017-04-05 17:16:45 +0200

    rajout simploniens nav bar + effets

--- effets.js
+++ effets.js
@@ -27,3 +27,22 @@ $(document).ready(function () {
         }
     });
 });
+
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navigation"});   
+
+  $("#myNavbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+     
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+            });
+    }  
+  });
+});
